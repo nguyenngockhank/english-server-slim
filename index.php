a4885d6ed54eda856578d96d1ccff684938da9e0
Apple <apple@Khank.local>
2019-02-10 17:56:08 +0700

    More api

--- index.php
+++ index.php
@@ -32,4 +32,33 @@ $app->get('/search_rhyme/{keyword}', function ($request, $response, array $args)
     return $response->withJson($list->toArray()); 
 });
 
+$app->post('/search_needle', function($request,  $response) use ($app) {
+
+    $body = $request->getParsedBody();
+    $chars = isset($body['chars']) ? $body['chars'] : [];
+
+    if(empty($chars)) {
+        return $response->withJson([]); 
+    }
+
+    $chars = array_map(function($char){
+        // check in A, a
+        $char = strtolower($char);
+        $ch_code = ord($char);
+        if($char === '' || $ch_code < ord('a') || $ch_code > ord('z')) {
+            return '_';
+        }
+        return $char;
+    },  $chars);
+
+    $condition = implode('', $chars);
+
+    /// load on demands
+    $app->getContainer()->get('db');
+
+
+    $list = Word::where('word', 'LIKE', $condition)->get();
+    return $response->withJson($list->toArray()); 
+});
+
 $app->run();
